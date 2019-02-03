26261e4bc5e0cdf43ec3564efdec13af731dcf1f
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-02-03 05:57:04 +0530

    Add-money and Wallet integrated with Backend

--- app/src/main/java/com/unknown_borrower/AddMoney.java
+++ app/src/main/java/com/unknown_borrower/AddMoney.java
@@ -1,37 +1,111 @@
 package com.unknown_borrower;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddMoney extends AppCompatActivity {
 
     private EditText amountText;
     private Button submitButton;
+
+    private TextView availableBalance;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_money);
 
-        amountText = (EditText) findViewById(R.id.amount);
+        availableBalance = findViewById(R.id.availablebalance);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        viewAmount();
+
+        amountText = (EditText) findViewById(R.id.addAmount);
         submitButton = (Button) findViewById(R.id.submit);
 
         submitButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addMoneyButtonClicked();
+                try {
+                    addMoneyButtonClicked();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    public void addMoneyButtonClicked()
+    private void viewAmount()
     {
-        String amt = amountText.getText().toString();
+        String url = "http://unknownborrowersbk-dev.us-east-1.elasticbeanstalk.com/profile/getProfile";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        try
+                        {
+                            Double amount = (Double) response.getDouble("balance");
+
+                            availableBalance.append("Available Balance:  \u20B9" + String.valueOf(amount));
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        })
+
+        {
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ5MTQ2MzQ1LCJleHAiOjE1NDkyMzI3NDV9.MXE0eZ32_l8O9GvOCgBLwks2hkudl-48OgeyNpKwcLA");
+                return headers;
+            }
+        };
+
+        mQueue.add(request);
+    }
+
+    public void addMoneyButtonClicked() throws JSONException {
+        final String amt = amountText.getText().toString();
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("amount",Double.parseDouble(amt));
+        final String requestString = jsonBody.toString();
 
         if(amt.length() == 0)
         {
@@ -55,12 +129,58 @@ public class AddMoney extends AppCompatActivity {
         }
         else
         {
+            String url = "http://unknownborrowersbk-dev.us-east-1.elasticbeanstalk.com/profile/addMoney";
+
+            StringRequest request = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.e("Your Array Response", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("error is ", "" + error);
+                }
+            }) {
+
+                //This is for Headers If You Needed
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json; charset=UTF-8");
+                    headers.put("Authorization", "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ5MTQ2MzQ1LCJleHAiOjE1NDkyMzI3NDV9.MXE0eZ32_l8O9GvOCgBLwks2hkudl-48OgeyNpKwcLA");
+                    return headers;
+                }
+
+                //Pass Your Parameters here
+                // @Override
+//                protected Map<String, String> getParams() {
+//                    Map<String, Double> params = new HashMap<String, Double>();
+//                    params.put("balance", Double.parseDouble(amt));
+//                    Map<String,String> returnParams = new HashMap<>();
+//                    returnParams.put("balance",amt);
+//                    return returnParams;
+//                }
+
+                @Override
+                public byte[] getBody() {
+                    try{
+                        return requestString.getBytes();
+                    }catch (Exception e){
+                        return null;
+                    }
+                }
+            };
+
+            mQueue.add(request);
+
             View linearLayout =  findViewById(R.id.info);
             //LinearLayout layout = (LinearLayout) findViewById(R.id.info);
 
 
             TextView valueTV = new TextView(this);
             valueTV.setText("Money Added succesfully");
+            valueTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
             valueTV.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.FILL_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT));
@@ -69,3 +189,4 @@ public class AddMoney extends AppCompatActivity {
         }
     }
 }
+
--- app/src/main/java/com/unknown_borrower/Wallet.java
+++ app/src/main/java/com/unknown_borrower/Wallet.java
@@ -1,19 +1,45 @@
 package com.unknown_borrower;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarException;
+
 
 public class Wallet extends AppCompatActivity {
+
+    private TextView balance, username;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
 
+        balance = findViewById(R.id.balance);
+        username = findViewById(R.id.username);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        viewUser();
 
         Button button = (Button) findViewById(R.id.add_money);
 
@@ -26,4 +52,49 @@ public class Wallet extends AppCompatActivity {
             }
         });
     }
+
+    String url = "http://unknownborrowersbk-dev.us-east-1.elasticbeanstalk.com/profile/getProfile";
+
+    private void viewUser()
+    {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        try
+                        {
+                            String name = response.getString("name");
+                            username.append(name);
+
+                            Double amount = (Double) response.getDouble("balance");
+                            balance.append("\u20B9 " + String.valueOf(amount));
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        })
+
+        {
+            @Override
+            public Map getHeaders() throws AuthFailureError
+            {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTQ5MTQ2MzQ1LCJleHAiOjE1NDkyMzI3NDV9.MXE0eZ32_l8O9GvOCgBLwks2hkudl-48OgeyNpKwcLA");
+                return headers;
+            }
+        };
+
+        mQueue.add(request);
+    };
 }
