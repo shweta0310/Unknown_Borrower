f91c9d8f0e0cdea31395f9d4ba7322d10e408425
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-04-03 01:31:48 +0530

    Image Upload Front-end added

--- MyFirstApp/app/src/main/java/com/example/niwedita/myfirstapp/editProfileActivity.java
+++ MyFirstApp/app/src/main/java/com/example/niwedita/myfirstapp/editProfileActivity.java
@@ -1,7 +1,14 @@
 package com.example.niwedita.myfirstapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.TypedValue;
@@ -19,6 +26,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,13 +35,16 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class editProfileActivity extends AppCompatActivity {
 
     private EditText name, city, state, org, age, gender;
     private TextView contactNum,email;
     private RequestQueue mQueue;
-
+    private CircleImageView photo;
     String token=null;
+    private Uri mainImageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +60,73 @@ public class editProfileActivity extends AppCompatActivity {
         org = findViewById(R.id.org_field);
         city = findViewById(R.id.city_field);
         state = findViewById(R.id.state_field);
-
+        photo = findViewById(R.id.image_upload);
         contactNum = findViewById(R.id.contactNum_field);
         email = findViewById(R.id.email_field);
 
         mQueue = Volley.newRequestQueue(this);
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    if (ContextCompat.checkSelfPermission(editProfileActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        //Toast.makeText(SetupActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(editProfileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                    } else {
+
+                        BringImagePicker();
+
+                    }
+
+                } else {
+                    BringImagePicker();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            BringImagePicker();
+        }
+    }
+
+    private void BringImagePicker() {
+
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(120, 160)
+                .start(editProfileActivity.this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mainImageURI = result.getUri();
+                photo.setImageURI(mainImageURI);
+
+                //isChanged = true;
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+
+            }
+        }
+
     }
 
     @Override
