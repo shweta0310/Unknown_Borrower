132db25a376bc99d9ae650e4ac31996dd9a1377b
rounak06 <rounak.modi06@gmail.com>
2019-04-03 12:46:45 +0530

    transaction and ratings

--- app/src/main/java/com/example/niwedita/myfirstapp/ratings.java
+++ app/src/main/java/com/example/niwedita/myfirstapp/ratings.java
@@ -1,7 +1,11 @@
 package com.example.niwedita.myfirstapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +14,7 @@ public class ratings extends AppCompatActivity {
 
     RatingBar rb;
     TextView value;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,28 @@ public class ratings extends AppCompatActivity {
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
               //  value.setText("Rated: "+rating);
                 Toast.makeText(ratings.this,
-                        "Rating changed, current rating "+ rb.getRating(),
+                        "Current rating "+ rb.getRating(),
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        bt = (Button) findViewById(R.id.submitratings);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ratings.this,R.style.MyDialogTheme);
+                builder.setMessage("Confirm Ratings?")
+                        .setPositiveButton("Yes",null)
+
+                        .setNegativeButton("No",null);
+
+            AlertDialog alert= builder.create();
+            alert.show();
+
+            }
+        });
+
 
     }
 }
